d66007326252751b53d60d3564b010aab48c0565
Rosa Pavón <mrosapavon@gmail.com>
2021-06-23 19:51:47 +0200

    cambiado

--- src/App.js
+++ src/App.js
@@ -9,22 +9,42 @@ function App() {
   const [data, setData]=useState([])
   const [sets, setSets]=useState('')
   const [data2, setData2]=useState([])
-  
 
+  function Carta(props){
+    const[cartasset, setCartaset]=useState([])
+    useEffect(() =>{
+      Promise.all(props.urls.map((urll) => fetch(urll)))
+      .then((respuesta)=> Promise.all(respuesta.map((res)=> res.json())))
+      .then((datos)=>{
+        setCartaset(datos)
+      })
+    },[setData2])
+ 
+    
+  const cartaHTML=cartasset.map((cartafinal)=>{
+    return <li>{cartafinal}</li>
+  })
+  return <ul>{cartaHTML}</ul>
+
+}
 
   useEffect(()=>{
     setLoading(true)
-    fetch(url).then(res => res.json()).then((datos)=>setData(datos.results))
+    fetch(url).then(res => res.json()).then((datos)=>setData(datos.sets))
     setLoading(false)
   },[url])
 
   useEffect(()=>{
     setLoading(true)
-    fetch(`https://api.magicthegathering.io/v1/sets/${sets}`).then(res => res.json()).then((datos)=>setData2(datos.code))
+    fetch(`https://api.magicthegathering.io/v1/cards?set=${sets}`).then(res => res.json()).then((datos)=>setData2(datos.cards))
     setLoading(false)
   },[sets])
+  
 
-  if(loading){
+  let setGuardado=data.map((cards) => {return(
+  <option key={cards.id} value={cards.code}>{cards.name}</option>)})
+
+   if(loading){
     return <h2>Cargando....</h2>
   }else{
 
@@ -32,23 +52,13 @@ function App() {
     <>
      <h1>Cartas</h1>
      <select onChange={ (e) => setSets(e.target.value)}>{/* aqui se muestra el que ha elegido el usuario y que esta en planeta */}
-      {data.map((cards) =>
-      <option key={cards.id} value={cards.name}>{cards.name}</option>)}     
+      {setGuardado}     
       
       </select>
       <h1>{sets}</h1>
+      <Carta urls={data2}/>
      
-      <ul>
-     {data.map((data2, index)=>{
-       if(index<=99){//esto condiciona que solo se puedan mostrar 10 elementos
-        return <li key={data2.id}>{data2.name},<br/><p>{data2.set}</p><br/><img src={data2.imageUrl}></img></li>
-
-       }
-       
-     })}
-   </ul>
-
-    </> 
+  </>
   );
     }
 }
